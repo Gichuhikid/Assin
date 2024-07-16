83197dd1ba95e9fc9c458a0db7126aee5338f7f7
gichuhikid <njegichuhi@gmail.com>
2024-07-16 22:34:42 +0300

    Orange HRM

---
+++ tests/test-5.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading')).toContainText('Dashboard');
+  await expect(page.locator('#app')).toContainText('Time at Work');
+  await page.getByRole('link', { name: 'Dashboard' }).click();
+  await expect(page.getByText('This Week')).toBeVisible();
+  await page.getByRole('link', { name: 'Leave' }).click();
+  await expect(page.getByRole('link', { name: 'Apply' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'My Leave' })).toBeVisible();
+  await expect(page.getByText('Entitlements')).toBeVisible();
+  await expect(page.getByText('Reports')).toBeVisible();
+  await expect(page.getByText('Configure')).toBeVisible();
+});
